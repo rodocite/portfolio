d13915d8f9f90cb8e13599bf7dfb52992b91f231
Rodolfo Yabut <rodolfoyabut@gmail.com>
2018-03-14 13:56:55 -0700

    Add a background to show scroll effects

--- pages/projects/fancy-scroll.js
+++ pages/projects/fancy-scroll.js
@@ -2,10 +2,14 @@ import Layout from '../../components/Layout.js'
 import Copy from '../../components/Copy'
 import styled from 'styled-components'
 
+const Background = styled.div`
+  width: 100%;
+  height: 300px;
+  background: black;
+`
+
 export default ({ children }) => (
   <Layout>
-    <Copy>
-      <p></p>
-    </Copy>
+    <Background />
   </Layout>
 )
